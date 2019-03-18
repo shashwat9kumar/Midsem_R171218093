44e0443ab02c666e38d68ee141c70b0eeb71ec46
SHASHWAT KUMAR <500068392@DDN.UPES.AC.IN>
2019-03-18 12:54:43 +0530

    add and subtract functions added to program

--- Midsem_R171218093/program.cpp
+++ Midsem_R171218093/program.cpp
@@ -13,9 +13,23 @@ class MidSem
 		y=b;
 	}
 
+	int add()
+	{
+		return (x+y);              // creation of function int add(commit 2)
+	}
+
+	int subtract()
+	{
+		return (x-y);               //creation of function int subtract(commit 3)
+	}
+
+
 };
 int main()
 {
 	MidSem obj(2,3);      // creation of object(commit 1)
+
+	cout<<obj.add()<<endl;
+	cout<<obj.subtract()<<endl;        //commit 2
 	return 0;
 }
